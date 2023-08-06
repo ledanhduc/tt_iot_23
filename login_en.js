2c3ab47b4e3e056fabbc96dad28a74856939c071
Lê Danh Đức <130637756+ledanhduc@users.noreply.github.com>
2023-08-06 17:04:38 +0700

    Update remember user and un remember

--- login_en.js
+++ login_en.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
@@ -33,6 +32,9 @@ const checkLoggedIn = () => {
     });
 };
 
+const checkbox = document.getElementById('rmb_ac');
+let userCredential; 
+
 const signup = async (e) => {
     e.preventDefault();
     const iddevice = document.getElementById("iddevice").value;
@@ -45,7 +47,6 @@ const signup = async (e) => {
         alert("Sign up successful");
         const encodedEmail = encodeURIComponent(email_reg.replace(/[.@]/g, '_'));
         await set(ref(db, `${encodedEmail}`), iddevice);
-        console.log("Lưu thông tin đăng ký vào Firebase thành công");
 
         if (await checkLoggedIn()) {
             window.location.replace("login_en.html");
@@ -61,7 +62,13 @@ const login = async (e) => {
     const pass_sig = document.getElementById("pass_sig").value;
 
     try {
+        userCredential = await signInWithEmailAndPassword(auth, email_sig, pass_sig); 
         await signInWithEmailAndPassword(auth, email_sig, pass_sig);
+        if (checkbox.checked) {
+        localStorage.setItem('user', JSON.stringify(userCredential.user));
+        } else {
+            localStorage.clear();
+        }
         window.location.replace("analytics_en.html");
     } catch (error) {
         alert("Sign in failed: " + error.message);
@@ -73,6 +80,26 @@ checkLoggedIn().then((isLoggedIn) => {
         window.location.replace("analytics_en.html");
     }
 });
+
+const user = JSON.parse(localStorage.getItem('user'));
+
+if (user) {
+  try {
+    auth.signInWithEmailAndPassword(user.email, user.password)
+  }
+  catch(error){
+      console.error(error);
+    };
+}
+
+if (user === null) {
+    try {
+        auth.signOut();
+    }
+    catch(error){
+        console.error(error);
+      };
+  }
 
 registerForm.addEventListener('submit', signup);
 loginForm.addEventListener('submit', login);
