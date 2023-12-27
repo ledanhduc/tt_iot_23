e510e7c1b7baca4c16b76fa38a085278c317df8d
Lê Danh Đức <130637756+ledanhduc@users.noreply.github.com>
2023-12-27 18:54:16 +0700

    Update toggle_control_en.js

--- toggle_control_en.js
+++ toggle_control_en.js
@@ -80,7 +80,6 @@ lamps_fb.forEach(function(lamp_fb) {
       lamp_fb.toggle.parentNode.classList.remove('active');
       lamp_fb.state.innerHTML = "OFF";
       lamp_fb.state.style.color = "rgb(227, 4, 90)";
-      set(ref(database, `${value}/st_timer`), false); 
     }
   });
 });
